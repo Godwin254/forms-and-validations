2ed8c6602baf75fd367c8b6a58422be7109f026c
Godwin254 <godwinbee15@gmail.com>
2020-03-27 00:58:09 -0400

    validating user inputs

--- 1-User-registration/validate.js
+++ 1-User-registration/validate.js
@@ -1,43 +1,47 @@
-//Validating our form---
+//variable declaration
+let form = document.getElementById("form1");
+let congrats_msg = document.querySelector(".congrats");
+let  name1 = document.getElementById("user-name");
+let  email = document.getElementById("email1");
+let pswd1 = document.getElementById("psw1");
+let pswd2 = document.getElementById("psw2");
+let success = document.querySelector(".fa-check-circle");
+let error_alert = document.querySelector(".alert-msg");
+let title = document.querySelector(".congrats");
+let btn = document.getElementById('btn1');
 
-function validate(){
-     var x = document.form["myform"][fname].value;
-     if(x == ""){
-          alert("fill in first name")
-          return false
+
+//prevent the form from submiting when empty
+     //form.addEventListener('submit', (e) => {
+         // e.preventDefault();
+
+        //  verify();
+     //});
+
+//add eventListner to perform a function on the form
+     //form.addEventListener('submit',  verify);
+
+     // define the function verify the form
+    function  verify(){
+
+          const nameValue = name1.value.trim();
+          const emailValue = email.value.trim();
+          const pas1Value = pswd1.value.trim();
+          const pas2Value = pswd2.value.trim();
+
+          //validate username input
+          if (nameValue === ' ' ){
+              //show error msg
+               alert("input name")
+          }else{
+               //show success class
+          }
+
+         
+         
+         //END 
      }
-}
-const fnameValue = fname.value.trim();
-const lnameValue = lname.value.trim();
-const dateValue = date.value.trim();
-const errorValue = email.value.trim();
-const countryValue = country.value.trim();
-const lineCheck = mobile.value.trim();
-const passwordValue = password.value.trim();
-const password2Value = password2.value.trim();
+     verify();
 
 
-if (usernameValue === '' ){
-     //show error
-     //add error class
-     setErrorFor(username, "username can not be blank")
-}else{
-     //add success class
-     setSuccessFor(username);
-}
 
-function setErrorFor(input, message){
-     const formControl = input.parentELement;
-     const small = formControl.querySelector(small);
-
-     //add error message inside
-     small.innertext = message;
-
-     //add error class
-     formControl.className = 'input-control error'
-}
-
-function setErrorFor(input){
-     const formControl = input.parentELement;
-     formControl.className = 'input-control error sucess';
-}    
